a4905e72464e4112ec15a2c100e80595151941e6
Brandon Wagner <brandon.wagner@teamaol.com>
2016-10-01 10:24:27 -0400

    kjsdf

--- os161-1.11/kern/thread/synch.c
+++ os161-1.11/kern/thread/synch.c
@@ -143,7 +143,7 @@ lock_acquire(struct lock *lock)
 
 	spl = splhigh();
 	while (lock->value==1) {
-	 	// thread_sleep(curthread->t_sleepaddr);
+	 	thread_sleep(curthread->t_sleepaddr);
 	}
 	// assert(lock->value == 0);
 	// lock->value = 1;
